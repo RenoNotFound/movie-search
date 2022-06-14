505a50692c0eaf82e9605f872c3086f625411d3e
Renato Gamper <gamper.renato@gmail.com>
2022-06-14 15:26:48 +0200

    set up apollo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 import "./index.css";
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "https://tmdb.sandbox.zoosh.ie/dev/grphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
