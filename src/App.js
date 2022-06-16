75f1a87720f35a1c8b6a2ccf4d25b1e0c8802174
Renato Gamper <gamper.renato@gmail.com>
2022-06-16 11:30:26 +0200

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
+import Movies from "./components/Movies";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MovieList />} />
+      <Route path="movies" element={<Movies />}>
+        <Route path=":movieId" element={<Movie />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
