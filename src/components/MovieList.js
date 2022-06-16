d4624ef722db2d3e042748157ab188711ae00017
Renato Gamper <gamper.renato@gmail.com>
2022-06-16 11:31:52 +0200

    create MovieList search for movies by name input and display data

---
+++ src/components/MovieList.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useLazyQuery, gql } from "@apollo/client";
+
+const SEARCH_MOVIES = gql`
+  query SearchMovies($title: String!) {
+    searchMovies(query: $title) {
+      id
+      name
+      score
+      genres {
+        name
+      }
+      overview
+      releaseDate
+    }
+  }
+`;
+
+const MovieList = () => {
+  const [movieTitle, setMovieTitle] = useState("");
+  const [executeSearch, { loading, error, data }] = useLazyQuery(SEARCH_MOVIES);
+
+  const handleSearch = () => {
+    executeSearch({ variables: { title: movieTitle } });
+  };
+
+  //   if (loading) return <p>Loading...</p>;
+  //   if (error) return <p>Error :(</p>;
+  return (
+    <>
+      <div>
+        Search
+        <input
+          type="text"
+          onChange={(event) => setMovieTitle(event.target.value)}
+          onKeyDown={(event) => {
+            event.key === "Enter" && handleSearch();
+          }}
+        />
+        <button onClick={handleSearch}>OK</button>
+      </div>
+      <ul>
+        {data ? (
+          <>
+            {data.searchMovies.map((movie) => (
+              <li key={movie.id}>
+                <p>{movie.name}</p>
+                <span>Score: {movie.score}</span>
+                <div>
+                  {" "}
+                  Categories:
+                  {movie.genres.map((genre) => (
+                    <span key={genre.name}>{genre.name}</span>
+                  ))}
+                </div>
+              </li>
+            ))}
+          </>
+        ) : loading ? (
+          <p>Loading...</p>
+        ) : (
+          ""
+        )}
+      </ul>
+    </>
+  );
+};
+
+export default MovieList;
